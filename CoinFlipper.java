f005792bfc3dfe5669cb6c3e90130b6e0e3efea6
PhobiaGH <137733026+PhobiaGH@users.noreply.github.com>
2023-06-25 19:40:47 -0400

    Add files via upload

---
+++ CoinFlipper.java
@@ -0,0 +1,10 @@
+public class CoinFlipper {
+  public static void main(String[] args) {
+  	String Heads = "Heads";
+    String Tails = "Tails";
+    int randomNum = (int)(Math.random() * 2);
+    
+    String result = (randomNum > 0) ? Heads : Tails;
+	  System.out.println(result);
+  }
+}
